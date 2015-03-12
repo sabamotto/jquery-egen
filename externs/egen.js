42b866fa6838262a72e7cda6d16ec65f20751f4d
SabaMotto <sabapotcom@yahoo.co.jp>
2015-03-12 14:24:14 +0900

    separate extern scripts of ClosureCompiler

---
+++ externs/egen.js
@@ -0,0 +1,3 @@
+// eGen
+jQuery.fn.egen = jQuery.fn.eGen = function(queryTokens, attrs, innerNodes){};
+jQuery.prototype.egen = jQuery.prototype.eGen = function(queryTokens, attrs, innerNodes){};
--- externs/others.js
+++ externs/others.js
@@ -4,7 +4,3 @@ define.amd;
 
 // Common.js
 var require = function(lib){};
-
-// eGen
-jQuery.fn.egen = jQuery.fn.eGen = function(queryTokens, attrs, innerNodes){};
-jQuery.prototype.egen = jQuery.prototype.eGen = function(queryTokens, attrs, innerNodes){};
